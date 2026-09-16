dfce046db1123b0d86f5a2398d26b7c836777aae
M4uSpY <maudelab23@gmail.com>
2026-09-16 12:17:31 -0400

    CLS-15: CRUD de materias con horas semanales y tipo de aula

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const userRoutes = require('./user.routes');
-const authRoutes = require('./auth.routes');
-const classroomRoutes = require('./classroom.routes');
-
+const express = require("express");
+const userRoutes = require("./user.routes");
+const authRoutes = require("./auth.routes");
+const classroomRoutes = require("./classroom.routes");
 const subjectRoutes = require("./subject.routes");
 
 const router = express.Router();
 
-
-router.use('/user', userRoutes);
-router.use('/auth', authRoutes);
-router.use('/classrooms', classroomRoutes);
+router.use("/user", userRoutes);
+router.use("/auth", authRoutes);
+router.use("/classrooms", classroomRoutes);
 
 router.use("/subjects", subjectRoutes);
 
