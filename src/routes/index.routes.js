4f0d0243c8ce5470ce596d2982d0fb692028e5f4
ChrisCore1 <ChrisCore1@users.noreply.github.com>
2026-09-17 16:40:22 -0400

    CLS-9: Fix conflicts

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const userRoutes = require('./user.routes');
 const authRoutes = require('./auth.routes');
 const classroomRoutes = require('./classroom.routes');
-<<<<<<< HEAD
+const teacherRoutes = require('./teacher.routes');
+
 const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = express.Router();
@@ -11,12 +12,6 @@ router.use('/auth', authRoutes);
 
 router.use(authMiddleware.verifyToken);
 
-=======
-const teacherRoutes = require('./teacher.routes');
-
-const router = express.Router();
-
->>>>>>> 068687a (CLS-9: CRUD de docentes con tope de horas semanales)
 router.use('/user', userRoutes);
 router.use('/classrooms', classroomRoutes);
 router.use('/teachers', teacherRoutes);
