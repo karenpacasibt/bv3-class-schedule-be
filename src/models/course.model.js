fe2f3582aed2a760c70a370a4c4ef91532bb85dc
karenpacasibt <romina.pacasi.btsoft@gmail.com>
2026-09-22 15:50:15 -0400

    CLS-13 CRUD de cursos con cantidad de estudiantes (#12)

---
+++ src/models/course.model.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const Course = sequelize.define(
+    "Course",
+    {
+      id: {
+        type: DataTypes.CHAR(26),
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      school_id: {
+        type: DataTypes.CHAR(26),
+        allowNull: false,
+      },
+
+      name: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+
+      student_count: {
+        type: DataTypes.SMALLINT.UNSIGNED,
+        allowNull: false,
+        validate: {
+          isInt: {
+            msg: "The number of students must be an integer.",
+          },
+          min: {
+            args: [1],
+            msg: "The number of students must be greater than zero.",
+          },
+        },
+      },
+    },
+    {
+      tableName: "courses",
+      timestamps: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+      deletedAt: "deleted_at",
+      paranoid: true,
+    }
+  );
+
+  return Course;
+};
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const userRoutes = require("./user.routes");
-const authRoutes = require("./auth.routes");
-const classroomRoutes = require("./classroom.routes");
-const subjectRoutes = require("./subject.routes");
-const authMiddleware = require("../middlewares/auth.middleware");
+const express = require('express');
+const userRoutes = require('./user.routes');
+const authRoutes = require('./auth.routes');
+const classroomRoutes = require('./classroom.routes');
+const teacherRoutes = require('./teacher.routes');
+const courseRoutes = require('./course.routes');
+
+const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
@@ -11,8 +13,9 @@ router.use("/auth", authRoutes);
 
 router.use(authMiddleware.verifyToken);
 
-router.use("/user", userRoutes);
-router.use("/classrooms", classroomRoutes);
-router.use("/subjects", subjectRoutes);
+router.use('/user', userRoutes);
+router.use('/classrooms', classroomRoutes);
+router.use('/teachers', teacherRoutes);
+router.use('/courses', courseRoutes);
 
 module.exports = router;
