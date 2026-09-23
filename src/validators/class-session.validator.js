c840178e5e7744a1ebd64d49bb1f6c9811c88db0
ChrisCore1 <ChrisCore1@users.noreply.github.com>
2026-09-23 15:35:27 -0400

    CLS-21: Rechazar clases por capacidad del aula, tipo de aula o tope del docente

--- src/validators/class-session.validator.js
+++ src/validators/class-session.validator.js
@@ -1,6 +1,31 @@
-function validateClassSession() {
-  return null;
-}
+const checkAvailability = require('./availability.validator');
+const checkRules = require('./rules.validator');
+
+const validateClassSession = async (data, classSessionId = null, schoolId = null) => {
+    const rulesError = await checkRules({
+        course_id: data.course_id,
+        classroom_id: data.classroom_id,
+        subject_id: data.subject_id,
+        teacher_id: data.teacher_id,
+        time_slot_id: data.time_slot_id,
+        school_id: schoolId,
+        session_id: classSessionId
+    });
+
+    if (rulesError) {
+        return rulesError;
+    }
+
+    return checkAvailability({
+        classroom_id: data.classroom_id,
+        teacher_id: data.teacher_id,
+        course_id: data.course_id,
+        day: data.day,
+        time_slot_id: data.time_slot_id,
+        school_id: schoolId,
+        classSessionId,
+    });
+};
 
 module.exports = validateClassSession;
 module.exports.validateClassSession = validateClassSession;
